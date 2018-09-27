e922c967d44b161a3d321daac41c7124d78443d5
Aditya12T <adityatripathi12@gmail.com>
2018-09-27 16:48:03 -0400

    Initial commit

---
+++ src/main/java/com/example/rest/webserv/restfulwebservice/RestfulWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.rest.webserv.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebserviceApplication.class, args);
+	}
+}
