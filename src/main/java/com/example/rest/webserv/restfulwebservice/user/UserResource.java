89f03e285b07b4211072aa4a02717b0235f47734
Aditya12T <adityatripathi12@gmail.com>
2018-09-28 17:02:05 -0400

    Post Save

--- src/main/java/com/example/rest/webserv/restfulwebservice/user/UserResource.java
+++ src/main/java/com/example/rest/webserv/restfulwebservice/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.example.rest.webserv.restfulwebservice.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -24,7 +27,13 @@ public class UserResource {
 		return service.findOne(id);
 	}
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+		.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+		
 	}
 }
