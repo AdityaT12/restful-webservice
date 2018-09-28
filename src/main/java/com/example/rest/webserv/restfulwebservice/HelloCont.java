8114bcaa8e69d524f3c7216ebe5595f25b3db4c7
Aditya12T <adityatripathi12@gmail.com>
2018-09-28 14:03:50 -0400

    helloPath

--- src/main/java/com/example/rest/webserv/restfulwebservice/HelloCont.java
+++ src/main/java/com/example/rest/webserv/restfulwebservice/HelloCont.java
@@ -1,6 +1,7 @@
 package com.example.rest.webserv.restfulwebservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloCont {
 	@GetMapping(path = "/hello-bean")
 	public HelloBean helloBean() {
 		return new HelloBean("Hello");
+	}
+	
+	@GetMapping(path = "/hello/path-variable/{name}")
+	public HelloBean helloPath(@PathVariable String name) {
+		return new HelloBean(String.format("Hello, %s", name));
 	}
 }
