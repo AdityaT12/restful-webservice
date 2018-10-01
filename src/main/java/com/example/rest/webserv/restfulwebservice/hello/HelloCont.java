7b16aa7a40602c9e07255c9a330b515c477dc507
Aditya12T <adityatripathi12@gmail.com>
2018-10-01 15:32:56 -0400

    Internationalized

--- src/main/java/com/example/rest/webserv/restfulwebservice/hello/HelloCont.java
+++ src/main/java/com/example/rest/webserv/restfulwebservice/hello/HelloCont.java
@@ -1,5 +1,10 @@
 package com.example.rest.webserv.restfulwebservice.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloCont {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(path = "/hello")
 	public String hello() {
 		return "Hello";
@@ -20,5 +28,10 @@ public class HelloCont {
 	@GetMapping(path = "/hello/path-variable/{name}")
 	public HelloBean helloPath(@PathVariable String name) {
 		return new HelloBean(String.format("Hello, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-int")
+	public String helloInt() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
